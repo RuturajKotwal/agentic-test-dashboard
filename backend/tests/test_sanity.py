304a2346f2af68cea98ef957c0f775c4c5b8ed2f
RuturajKotwal <ruturajkotwal@gmail.com>
2026-06-16 12:32:39 +0530

    Add github actions workflow for automated testing and builds

---
+++ backend/tests/test_sanity.py
@@ -0,0 +1,7 @@
+def test_environment_is_configured():
+    """
+    A basic sanity check to ensure Pytest is running correctly
+    in our CI pipeline.
+    """
+    assert 1 == 1
+    assert True is True
